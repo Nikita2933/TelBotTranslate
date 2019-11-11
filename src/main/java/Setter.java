5242b29640033aa83a4b0cb34c7e850d80f66c27
Nikita2933 <57300319+Nikita2933@users.noreply.github.com>
2019-11-12 00:33:20 +0300

    Update Setter.java

--- src/main/java/Setter.java
+++ src/main/java/Setter.java
@@ -1,6 +1,6 @@
 class Setter {
-    String apikey = "trnsl.1.1.20130918T211717Z.3ab20d41ced64e5d.224265dad05a5d229373fbeff6090f3c95107941" ;
-    String tokenBot = "941938837:AAECmY8cBEFG0JRxf58nITFiW0qjXgK_pzk";
-    String botname = "Bottest";
+    String apikey = "Your apikey" ;
+    String tokenBot = "Your tokenBot";
+    String botname = "Your botname";
 
 }
