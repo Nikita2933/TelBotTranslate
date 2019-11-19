a330d491f0ab4d1b90854d20ba4f2601f1a1803b
Nikita2933 <Smith100500@gmail.com>
2019-11-19 22:10:02 +0300

    Finish

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,3 +1,9 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
@@ -8,54 +14,96 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
 
-
-public  class Bot  extends TelegramLongPollingBot {
+public class Bot extends TelegramLongPollingBot {
     private Setter setter = new Setter();
     private Translate tr = new Translate();
-    public static void main(String[] args) {
 
+    public Bot() {
+    }
+
+    public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi botapi = new TelegramBotsApi();
+
         try {
             botapi.registerBot(new Bot());
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+        } catch (TelegramApiException var3) {
+            var3.printStackTrace();
         }
+
     }
-    private  void sendMsg(Message msg, String text) {
+
+    private void sendMsg(Message msg, String text) {
         SendMessage s = new SendMessage();
         s.setChatId(msg.getChatId());
         s.setText(text);
+
         try {
-            execute(s);
-        } catch (TelegramApiException e){
-            e.printStackTrace();
+            this.execute(s);
+        } catch (TelegramApiException var5) {
+            var5.printStackTrace();
         }
+
     }
-    @Override
+
     public String getBotUsername() {
-        return setter.botname ;
-
+        return this.setter.botname;
     }
-    @Override
+
     public String getBotToken() {
-        return setter.tokenBot;
-
+        return this.setter.tokenBot;
     }
 
-    @Override
     public void onUpdateReceived(Update e) {
         Message msg = e.getMessage();
         String txt = msg.getText();
-        try {
-            String eng = tr.getJsonStringYandex(txt);
-            sendMsg(msg,eng);
+        String eng = null;
+        switch (txt) {
+            case "/start":
+                eng = "Приветствую, для использования переводчика необходимо выбрать направление перевода /ruen с Русского на Английский  /enru - с Английского на Русский(По умолчанию /ruen) . Если Вам необходимо узнать значение слова введите /dictionary(анг. словарь)";
+                sendMsg(msg, eng);
+                break;
+            case "/ruen":
+                tr.url = this.setter.urlTrans;
+                tr.apiKey = this.setter.apikeytrans;
+                tr.lang = this.tr.translangruen;
 
-        } catch (IOException ex) {
 
-            ex.printStackTrace();
+                this.sendMsg(msg, "Выбран перевод с Русского на Английский");
+                break;
+            case "/enru":
+                tr.url = setter.urlTrans;
+                tr.apiKey = this.setter.apikeytrans;
+                tr.lang = this.tr.translangenru;
 
+
+                this.sendMsg(msg, "Выбран перевод с Английского на Русский");
+                break;
+            case "/dictionary":
+                tr.url = this.setter.urldict;
+                tr.apiKey = this.setter.apikeydict;
+                tr.lang = this.tr.dictlangru;
+                sendMsg(msg, "Выбран режим словаря: По умолчанию для изменения на Русский словарь команда /ChangeRu  )");
+
+                break;
+            case "/ChangeRu":
+                tr.url = this.setter.urldict;
+                tr.apiKey = this.setter.apikeydict;
+                tr.lang = this.tr.dictlangruru;
+                sendMsg(msg, "Выбран режим словаря: Русский словарь");
+
+                break;
+            default:
+                try {
+                    eng = this.tr.getJsonStringYandex(txt);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                sendMsg(msg, eng);
+                break;
         }
     }
+
 }
+
 
--- src/main/java/Translate.java
+++ src/main/java/Translate.java
@@ -1,55 +1,73 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
 
 class Translate {
     private Setter setter = new Setter();
+    String lang = "ru-en";
+    String translangruen = "ru-en";
+    String translangenru = "en-ru";
+    String dictlangru = "en-ru";
+    String dictlangruru = "ru-ru";
+    String url = this.setter.urlTrans;
+    String apiKey = this.setter.apikeytrans;
 
-    String getJsonStringYandex(String text) throws IOException {
-        String apiKey = setter.apikey;
-        String requestUrl = "https://translate.yandex.net/api/v1.5/tr.json/translate?key="
-                + apiKey + "&lang=" + "ru-en" + "&text=" + URLEncoder.encode(text, "UTF-8");
 
+    String getJsonStringYandex(String ex) throws IOException {
+        String requestUrl = url + apiKey + "&lang=" + this.lang + "&text=" + ex;
         URL url = new URL(requestUrl);
-        HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+        HttpURLConnection httpConnection = (HttpURLConnection)url.openConnection();
         httpConnection.connect();
         int rc = httpConnection.getResponseCode();
-
-        if (rc == 200) {
-            String line;
+        if (rc != 200) {
+            return "beard";
+        } else {
             BufferedReader buffReader = new BufferedReader(new InputStreamReader(httpConnection.getInputStream()));
             StringBuilder strBuilder = new StringBuilder();
-            while ((line = buffReader.readLine()) != null) {
+
+            String line;
+            while((line = buffReader.readLine()) != null) {
                 strBuilder.append(line).append('\n');
             }
 
-            return getTranslateFromJSON(strBuilder.toString());
+            return this.getTranslateFromJSON(strBuilder.toString());
         }
-
-        return "beard";
     }
 
-    private String getTranslateFromJSON(String str) {
+    String getTranslateFromJSON(String str) {
         JSONParser parser = new JSONParser();
         JSONObject object = null;
+
         try {
-            object = (JSONObject) parser.parse(str);
-        } catch (org.json.simple.parser.ParseException e) {
-            e.printStackTrace();
+            object = (JSONObject)parser.parse(str);
+        } catch (ParseException var8) {
+            var8.printStackTrace();
         }
+
         StringBuilder sb = new StringBuilder();
+
         assert object != null;
-        JSONArray array = (JSONArray) object.get("text");
+
+        JSONArray array = (JSONArray)object.get("text");
+
         for (Object s : array) {
             sb.append(s.toString()).append("\n");
         }
+
         return sb.toString();
     }
 }
+
+
